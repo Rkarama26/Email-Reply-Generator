dd63b788fe4e0425979378a133ac3db96516e74b
Rohit <rohit.karma026@gmail.com>
2025-01-25 16:07:02 +0530

    base version completed

--- email-writer/src/main/java/com/email_writer/EmailWriterApplication.java
+++ email-writer/src/main/java/com/email_writer/EmailWriterApplication.java
@@ -2,12 +2,20 @@ package com.email_writer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class EmailWriterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailWriterApplication.class, args);
+	}
+
+	@Bean
+
+	public WebClient webClient(WebClient.Builder builder) {
+		return builder.build(); // No base URL set
 	}
 
 }
