b8ab9a5305dde4dc3cc8c37f093c4a9627fa8ca2
Rohit <rohit.karma026@gmail.com>
2025-01-27 23:19:53 +0530

    email-writter-ui

--- email-writer/src/main/java/com/email_writer/controller/EmailGeneratorController.java
+++ email-writer/src/main/java/com/email_writer/controller/EmailGeneratorController.java
@@ -4,14 +4,13 @@ import com.email_writer.dto.EmailRequest;
 import com.email_writer.service.EmailGeneratorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 //api/email/generate
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
